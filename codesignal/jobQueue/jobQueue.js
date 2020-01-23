748dd819328e872b2ef2fddba8811507347ed93c
Gabriel <gabriel.laruentiu@securesystemdesign.io>
2020-01-23 20:21:05 +0200

    add codesignal test

---
+++ codesignal/jobQueue/jobQueue.js
@@ -0,0 +1,53 @@
+function createJobQueue() {
+    // TODO - create and return a job queue
+    let queue = [];
+
+    const addJob = job => {
+        let promise = function() {
+            return new Promise((resolve, reject) => {
+                try {
+                    const match = queue.filter(obj => obj.job === job);
+                    if (match !== undefined) {
+                        job().then(resolve).catch(reject);
+                    } else {
+                        reject();
+                    }
+                } catch(err) {
+                    reject(err);
+                }
+            });
+        }
+
+        queue.push({ job, promise });
+        return promise;
+    };
+
+    const cancelJob = job => {
+        for (let i = 0; i < queue.length; i++) {
+            if (job === queue[i].job) {
+                console.log('founded cancelling job');
+                queue.splice(i, 1);
+                return;
+            }
+        }
+    };
+
+    const processAllJobs = () => {
+        let success = 0;
+        return new Promise(async (resolve, reject) => {
+            for (let i = 0; i < queue.length; i++) {
+                try {
+                    await queue[i].promise();
+                    success++;
+                } catch(e) {}
+            }
+            resolve(success);
+        });
+    };
+
+    return {
+        addJob, cancelJob, processAllJobs
+    };
+}
+
+module.exports = { createJobQueue };
