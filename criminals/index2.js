b9b8158961e04481a455825420fa2831fb8924bd
Gabriel <gabriel.laruentiu@securesystemdesign.io>
2020-02-05 14:50:14 +0200

    update criminal solutions

--- criminals/index2.js
+++ criminals/index2.js
@@ -5,46 +5,40 @@ criminals.set("Paul White Jr.", null);
 criminals.set("Red Fortress", "Roger Rabbit, Ross Winter");
 criminals.set("Redford Fort", "Red Strong, Red Fort");
 
-/**
- * Search criminal by keyword
- * @param {String} keyword keyword to search criminal
- */
-function searchBy(keyword) {
-    let match_key = null, status = 0; // 0: No match, 1: Partial alias match, 2: Full alias match, 3: Partial name match, 4: Full name match
-    keyword = keyword.toLowerCase();
+function findCriminal(name) {
+    let name_partial_match = null,
+        name_full_match = null,
+        alias_partial_match = null,
+        alias_full_match = null;
+    name = name.toLowerCase();
 
-    for (let [key, value] of criminals) {
-        if (key.toLowerCase() == keyword) {
-            match_key = key;
-            status = 4;
-            break;
-        }
-
-        if (status < 3 && key.toLowerCase().includes(keyword)) {
-            match_key = key;
-            status = 3;
-        } else if (value) {
-            let aliases = value.toLowerCase().split(', ');
-            if (status < 2 && aliases.some(val => val == keyword)) {
-                match_key = key;
-                status = 2;
-            } else if (status < 1 && aliases.some(val => val.includes(keyword))) {
-                match_key = key;
-                status = 1;
+    criminals.forEach((value, key, map) => {
+        if (key.toLowerCase() == name) {
+            name_full_match = key + ': ' + map.get(key);
+        } else if (key.toLowerCase().includes(name)) {
+            name_partial_match = key + ': ' + map.get(key);
+        } else {
+            if (!value) {
+                return;
+            }
+            value = value.toLowerCase();
+            let aliases = value.split(', ');
+            if (aliases.some(val => val == name)) {
+                alias_full_match = key + ': ' + map.get(key);
+            } else if (aliases.some(val => val.includes(name))) {
+                alias_partial_match = key + ': ' + map.get(key);
             }
         }
-    }
+    });
 
-    return match_key ? match_key + ' => ' + criminals.get(match_key) : 'No match';
+    return name_full_match || name_partial_match || alias_full_match || alias_partial_match || 'No match';
 }
 
-const test1 = searchBy('paul White');
+const test1 = findCriminal('paul White');
 console.log(test1);
-const test2 = searchBy('Roger');
+const test2 = findCriminal('Roger');
 console.log(test2);
-const test3 = searchBy('Ross');
+const test3 = findCriminal('Ross');
 console.log(test3);
-const test4 = searchBy('white jr.');
+const test4 = findCriminal('white jr.');
 console.log(test4);
-const test5 = searchBy('white jr.1');
-console.log(test5);
